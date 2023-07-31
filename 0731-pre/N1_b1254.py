d06e6a11d352e02941084b5a051b0d7ad17349b4
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-07-31 15:58:54 +0900

    not yet

---
+++ 0731-pre/N1_b1254.py
@@ -0,0 +1,16 @@
+import sys
+
+input= sys.stdin.readline
+
+S= input()
+
+# for i in range(len(S)):
+#     if S[i:] == S[i:][::-1]:
+
+
+
+
+# Anyway, let's cross the str whether thats a palindrome
+str_size= len(S)
+print(S[:str_size]) 
+print(S[str_size+1:])
